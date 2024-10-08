ea42160c5cf0cedaffd0c4d66b50a9a5c65ed3d9
Daniel C. Opute <danielcopute@gmail.com>
2024-10-08 15:12:55 +0100

    added a prepare statement to getAnInventory

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,6 +4,7 @@ const baseController = {}
 baseController.buildHome = async function (req, res) {
     const nav = await utilities.getNav()
     const description = 'CSE 340 Backend Development Motor Web Application'
+    req.flash("notice", "This is a flash message.")  // for testing
     res.render("index", {title: "Home", nav, description})
 }
 
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -30,7 +30,13 @@ async function getInventoryByClassificationId(classification_id) {
 ******************************************************************/
 async function getAnInventory(inv_id) {
     try {
-        const data = await pool.query(`SELECT * FROM public.inventory WHERE inv_id = ${inv_id}`)
+        // using a prepare statement
+        const query = {
+            name: 'get-an-inventory',
+            text: 'SELECT * FROM public.inventory WHERE inv_id = $1',
+            values: [inv_id]
+        }
+        const data = await pool.query(query)
         return data.rows[0]
     } catch (error) {
         console.error("getAnInventory error " + error)
