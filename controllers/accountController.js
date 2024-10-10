fe6d176f2315f93a151bc3f287fee3e44b5b468b
Daniel C. Opute <danielcopute@gmail.com>
2024-10-10 23:39:44 +0100

    team-activity-completed-successfully.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,7 @@
 const utilities = require('../utilities/')
 const accountModel = require('../models/account-model')
+const bcrypt = require("bcryptjs")
+const { errors } = require('jshint/src/messages')
 
 /* ***************************************
 * Deliver Login View
@@ -11,6 +13,7 @@ async function buildLogin(req, res, next) {
         description: "Login Page",
         title: "Login",
         nav,
+        errors: null,
     })
 }
 
@@ -34,19 +37,35 @@ async function registerAccount(req, res) {
     let nav = await utilities.getNav()
     const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+    // Has the password before storing
+    let hashedPassword
+    try {
+        // regular password and cost (salt is generated automatically)
+        hashedPassword = await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+        req.flash("notice", 'Sorry, there was an error processing the registration.')
+        res.status(500).render("account/register", {
+            description: "Registration Page",
+            title: "Registration",
+            nav,
+            errors: null,
+        })
+    }
+
     const regResult = await accountModel.registerAccount (
         account_firstname,
         account_lastname,
         account_email,
-        account_password
+        hashedPassword
     )
 
     if (regResult) {
-        req.flash ("notice", `Congratulations, you are registered ${account_firstname.toUpperCase}. Please log in.`)
+        req.flash ("notice", `Congratulations, you are registered ${account_firstname}. Please log in.`)
         res.status(201).render("account/login", {
             description: "login page",
             title: "Login",
             nav,
+            errors: null
         })
     } else {
         req.flash ("notice", "Sorry, the registration failed.")
